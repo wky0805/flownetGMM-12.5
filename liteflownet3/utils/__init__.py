d796affd6e015bf310f317a05a94f4b9262c0479
wky <53677093+wky0805@users.noreply.github.com>
2025-11-13 07:44:58 +0300

    Add bilingual guidance for crowd analysis workflow

--- liteflownet3/utils/__init__.py
+++ liteflownet3/utils/__init__.py
@@ -1,6 +1,12 @@
 """Utility helpers for LiteFlowNet3."""
 
 from .config import OptimizerConfig, TrainingConfig
+from .crowd_analysis import (
+    CrowdGroup,
+    CrowdTracker,
+    analyse_sequence,
+    main as crowd_analysis_main,
+)
 from .train_utils import EndpointError, ensure_cuda_compat, save_checkpoint
 
 __all__ = [
@@ -9,4 +15,8 @@ __all__ = [
     "EndpointError",
     "ensure_cuda_compat",
     "save_checkpoint",
+    "CrowdGroup",
+    "CrowdTracker",
+    "analyse_sequence",
+    "crowd_analysis_main",
 ]
